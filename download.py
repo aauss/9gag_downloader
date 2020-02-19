b12b43281e13de3245cdfd9b3d59000aa357e8d7
Auss <a.abbood@live.de>
2020-02-20 00:59:28 +0100

    Add: click options and login process

--- download.py
+++ download.py
@@ -1,3 +1,4 @@
+import click
 import requests
 import time
 
@@ -8,17 +9,22 @@ from bs4 import BeautifulSoup
 
 driver = webdriver.Chrome(executable_path=r"/Users/aussabbood/github/9gag_downloader/chromedriver")
 
-def site_login():
+@click.command()
+@click.option("--email", "-e", help="your 9gag account email", type=str, prompt=True)
+@click.option("--password", "-p", help="your 9gag account password", type=str, prompt=True, hide_input=True)
+@click.option("--username", "-p", help="your 9gag username", type=str, prompt=True)
+def site_login(email, password, username):
     driver.get("https://9gag.com/")
-    driver.find_element_by_xpath("/html/body/div[7]/div[1]/div[2]/div/span").click()
-    driver.find_element_by_xpath("/html/body/header/div/div/div[2]/a[1]").click()
-    # driver.find_element(By.XPATH, '//button[text()="Got it, thanks!"]')
-    # driver.find_element_by_id("jsid-login-button").click()
-    # driver.find_element_by_id("username").send_keys("")
-    # driver.find_element_by_id("passwort").send_keys("")
-    # driver.find_element_by_id("submit").click()
-site_login()
-username = ""
-likes = requests.get(f"https://9gag.com/u/{username}/likes")
-soup = BeautifulSoup(likes.content)
-print(soup)
+    driver.find_element_by_xpath("/html/body/div[7]/div[1]/div[2]/div/span").click()  #dismiss cookie warning
+    time.sleep(0.5)
+    driver.find_element_by_xpath("/html/body/header/div/div/div[2]/a[1]").click()  #login button
+    time.sleep(0.5)
+    driver.find_element_by_xpath("/html/body/div[2]/div[3]/section/section/div/form/div[1]/input").send_keys(email)
+    driver.find_element_by_xpath("/html/body/div[2]/div[3]/section/section/div/form/div[2]/input").send_keys(password)
+    driver.find_element_by_xpath("/html/body/div[2]/div[3]/section/section/div/form/div[3]/input").click()
+    time.sleep(0.4)
+    driver.get(f"https://9gag.com/u/{username}/likes")
+
+
+if __name__=="__main__":
+    site_login()
